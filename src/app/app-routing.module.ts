6825b11d6cb49742ce9264d1aa6655a8b01f1ccd
Achmoye <achmoye@gmail.com>
2023-09-09 11:49:36 +0200

    added form to post message

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ThreadComponent } from './pages/thread/thread.component';
 import { ProfileComponent } from './pages/profile/profile.component';
+import { CategoryComponent } from './pages/category/category.component';
 
 const routes: Routes = [
   {path: "", component: HomeComponent},
   {path: "thread/:id", component: ThreadComponent},
-  {path: "profile/:id", component: ProfileComponent}
-
+  {path: "profile/:id", component: ProfileComponent},
+  {path: "category/:id", component: CategoryComponent},
 ];
 
 @NgModule({
--- src/app/pages/category/category.component.ts
+++ src/app/pages/category/category.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-category',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./category.component.scss']
 })
 export class CategoryComponent {
+  constructor(private http: HttpClient){
 
+  }
+  category$: Observable<any> = this.http.get("http://localhost:3000/category/1");
+
+  moderators$: Observable<any> = this.http.get("http://localhost:3000/moderator");
+  p: number = 1; // For ngx pagination 
 }
